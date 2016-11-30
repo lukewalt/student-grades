87628c7363174ff2b548bd682c6e11b19f3bceb8
Luke Woodward <luke.wdwrd@gmail.com>
2016-11-30 14:11:22 -0600

    created object that stores the number of times each grade appears in the array

--- grades.js
+++ grades.js
@@ -1,9 +1,11 @@
-var scores = [72, 78, 98, 98, 78, 62, 62, 78, 78, 50, 50, 72, 92];
+var scores = [72, 78, 98, 98, 78, 62, 62, 78, 78, 81, 50, 72, 85];
 
 //created empty array to hold each converted letter
 var letterGrade = [];
+
 for (var i = 0; i < scores.length; i++) {
-  var nu = scores.sort(function(a, b){return b-a});
+  var nu = scores.sort(function(a, b){return b-a});  //reverses array order
+
   //if statement pushes designated letter if the number meets the condition
   if (nu[i] >= 90) {
     letterGrade.push("A");
@@ -22,32 +24,41 @@ for (var i = 0; i < scores.length; i++) {
   }
 }
 
-
-
-
+// empty object to push number of each grade
 var tally = {
-    A: null,
-    B: null,
-    nickels: null,
-    pennies: null
-  };
+  a: 0,
+  b: 0,
+  c: 0,
+  d: 0,
+  f: 0
+};
 
+// loop that places the occurances of each letter grade with coresponding key of object
 for (var i = 0; i < letterGrade.length; i++) {
-  console.log(letterGrade[i]);
+
+  if (letterGrade[i] == "A"){
+    tally.a += 1;
+  } else if (letterGrade[i] == "B") {
+    tally.b += 1;
+  } else if (letterGrade[i] == "C") {
+    tally.c += 1;
+  } else if (letterGrade[i] == "D") {
+    tally.d += 1;
+  } else {
+    tally.f += 1;
+  }
+
 }
+// printing number of each letter grade to console
+console.log(tally);
 
-
-
-console.log("--------------------------")
-
-
-// What is the highest grade?
+// calculates highest grade
 var highestGrade = function( array ){
   return Math.max.apply( Math, array );
 };
 console.log("Highest Grade: " + highestGrade(scores));
 
-// What is the lowest grade?
+// calculates lowest grade?
 var highestGrade = function( array ){
   return Math.min.apply( Math, array );
 };
