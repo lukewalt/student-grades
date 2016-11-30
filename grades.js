35b28c1681d1820497a1e29aff65ebde8313154c
Luke Woodward <luke.wdwrd@gmail.com>
2016-11-30 09:34:27 -0600

    added max and min function to find highest and lowest value in array

--- grades.js
+++ grades.js
@@ -1,7 +1,23 @@
 var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
 
+
+var lowestGrade = Math.min(scores);
+
 // Use console.log to output the following criteria:
-//
-// How many of each grade?
-// What is the lowest grade?
+for (var i = 0; i < scores.length; i++) {
+  // How many of each grade?
+
+}
+
+
 // What is the highest grade?
+var highestGrade = function( array ){
+  return Math.max.apply( Math, array );
+};
+console.log(highestGrade(scores));
+
+// What is the lowest grade?
+var highestGrade = function( array ){
+  return Math.min.apply( Math, array );
+};
+console.log(highestGrade(scores));
