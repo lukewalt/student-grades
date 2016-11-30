92419aa9f44cc715022c3a5bd8ddb503863c8828
Luke Woodward <luke.wdwrd@gmail.com>
2016-11-30 12:43:50 -0600

    created empty array and stored letters from numeric values

--- grades.js
+++ grades.js
@@ -1,13 +1,30 @@
-var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+var scores = [92, 78, 98, 98, 78, 72, 72, 78, 78, 50, 50, 72, 92];
 
-
-var lowestGrade = Math.min(scores);
-
-// Use console.log to output the following criteria:
+//created empty array to hold each converted letter
+var grade = [];
 for (var i = 0; i < scores.length; i++) {
-  // How many of each grade?
-
+  var nu = scores.sort();
+  //if statement pushes designated letter if the number meets the condition
+  if (nu[i] >= 90) {
+    grade.push("A");
+  }
+  else if (nu[i] >= 80) {
+    grade.push("B");
+  }
+  else if (nu[i] >= 70) {
+    grade.push("C");
+  }
+  else if (nu[i] >= 60) {
+    grade.push("D");
+  }
+  else {
+    grade.push("F");
+  }
 }
+console.log(grade);
+
+
+console.log("--------------------------")
 
 
 // What is the highest grade?
